96e3c5629d1b00e1e706614ed69d96a478195e9f
James Li <james.li.post@protonmail.com>
2026-09-22 18:07:02 -0700

    Change GameState to unique bitwise values and add interacting
    Changes GameState to make it more usable through bitwise operations
    instead of repeated checks of multiple states. Also adds the interacting
    state when dealing with objects.

--- script/RuntimeState.gd
+++ script/RuntimeState.gd
@@ -2,13 +2,15 @@ extends Node
 
 # state machine
 enum GameState {
-	WORLD, # overworld interactions: BATTLE_WAITING | MENU --> WORLD --> BATTLE_SETUP | MENU
-	MENU, # BATTLE_MENU --> MENU --> BATTLE_MENU | WORLD
-	BATTLE_SETUP, # battle setup and turn resolution: WORLD --> BATTLE_SETUP --> 
-	BATTLE_MENU, # player choosing actions from menus: IN_BATTLE
-	BATTLE_PLAYER_ACTION, # mid attack/defense action: IN_BATTLE_MENU | IN_BATTLE_ENEMY_ACTION
-	BATTLE_ENEMY_ACTION, # AI action, player can respond
-	BATTLE_WAITING, # turn resolution, if all dead battle ends
+	NULL = 0,
+	WORLD = 1, # overworld interactions: BATTLE_WAITING | MENU --> WORLD --> BATTLE_SETUP | MENU
+	INTERACTING = 2, # MID INTERACTIOn
+	MENU = 4, # BATTLE_MENU --> MENU --> BATTLE_MENU | WORLD
+	BATTLE_SETUP = 8, # battle setup and turn resolution: WORLD --> BATTLE_SETUP -->
+	BATTLE_MENU = 16, # player choosing actions from menus: IN_BATTLE
+	BATTLE_PLAYER_ACTION = 32, # mid attack/defense action: IN_BATTLE_MENU | IN_BATTLE_ENEMY_ACTION
+	BATTLE_ENEMY_ACTION = 64, # AI action, player can respond
+	BATTLE_WAITING = 128, # turn resolution, if all dead battle ends
 }
 
 var current_game_state: GameState
